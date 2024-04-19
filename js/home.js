39c62c399c3dea3189bef0b820510311c9ce8c1b
NicolleMP <nimendespoltosi46@gmail.com>
2024-04-19 14:30:30 -0300

    add JSON files

---
+++ js/home.js
@@ -0,0 +1,62 @@
+let promocoes = [
+    {
+        "id": 1,
+        "nome": "Maçãs",
+        "imagem": "macas.jpg",
+        "desconto": "20"
+    },
+    {
+        "id": 2,
+        "nome": "Bananas",
+        "imagem": "bananas.jpg",
+        "desconto": "15"
+    },
+    {
+        "id": 3,
+        "nome": "Cenouras",
+        "imagem": "cenouras.jpg",
+        "desconto": "25"
+    },
+    {
+        "id": 4,
+        "nome": "Pães",
+        "imagem": "paes.jpg",
+        "desconto": "30"
+    },
+    {
+        "id": 5,
+        "nome": "Leite",
+        "imagem": "leite.jpg",
+        "desconto": "10"
+    },
+    {
+        "id": 6,
+        "nome": "Ovos",
+        "imagem": "ovos.jpg",
+        "desconto": "18"
+    },
+    {
+        "id": 7,
+        "nome": "Tomates",
+        "imagem": "tomates.jpg",
+        "desconto": "22"
+    },
+    {
+        "id": 8,
+        "nome": "Cereais",
+        "imagem": "cereais.jpg",
+        "desconto": "12"
+    },
+    {
+        "id": 9,
+        "nome": "Queijo",
+        "imagem": "queijo.jpg",
+        "desconto": "28"
+    },
+    {
+        "id": 10,
+        "nome": "Frango",
+        "imagem": "frango.jpg",
+        "desconto": "17"
+    }
+]
